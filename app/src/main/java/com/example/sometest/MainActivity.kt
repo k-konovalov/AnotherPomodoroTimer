b8f05ae6b3500a53bda3a9dac2460d989b8b41eb
Andrew <verannick@gmail.com>
2019-06-21 19:15:29 +0300

    Binding fix

--- app/src/main/java/com/example/sometest/MainActivity.kt
+++ app/src/main/java/com/example/sometest/MainActivity.kt
@@ -5,14 +5,16 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.example.sometest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //ActivityMainBinding - ребилд не помогает
-        //val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
