a8702c37dc38076ca742647b9774f9989eed474a
Andrew <verannick@gmail.com>
2019-06-21 15:48:01 +0300

    Binding
    К этому еще нужно будет вернуться и разобраться

--- app/src/main/java/com/example/sometest/MainActivity.kt
+++ app/src/main/java/com/example/sometest/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.sometest
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //ActivityMainBinding - ребилд не помогает
+        //val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
